3942c67bd1a0115263914893d01bdab9737ee339
Bubah Conteh <contehbub@gmail.com>
2020-06-12 11:48:19 -0400

    BC/RR - Setup initial project for MapReduce

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState<string>('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MapReduce</h1>
+      <textarea onChange={(e) => setText(e.target.value)} value={text}></textarea>
+      <button onClick={() => mapReduce(text)}>Submit</button>
     </div>
   );
+}
+
+// Handle empty string in mapReduce
+
+function mapReduce(inputString: string){
+  alert(inputString)
 }
 
 export default App;
